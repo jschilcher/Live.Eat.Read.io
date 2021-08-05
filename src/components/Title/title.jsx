9020d8db954c173e7cf805c83e6eff529ec1dc7f
Jenelle <jschilcher8@gmail.com>
2021-08-05 18:59:39 -0500

    Add title

---
+++ src/components/Title/title.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "../Title/title.css";
+
+function Title(props) {
+  return (
+    <div className="row row-spacer barcolor">
+      <div className="col-align">
+        <div className="title-nav">
+          <h1>Live.Eat.Read.</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Title;
--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
+import "./app.css"
+import Title from "./Title/title"
 
 function App() {
     return (
         <div className="App">
+            <Title/>
             <Switch>
 
             </Switch>
         </div>
     )
 }
+
+export default App;
