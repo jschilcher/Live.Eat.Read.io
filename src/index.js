fbc05bdfd1e76f2af697a4617471c7e347625615
Jenelle <jschilcher8@gmail.com>
2021-08-05 18:38:30 -0500

    Install and set up for React-Router-Dom

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import "../src/index.css";
 import App from "./components/app";
 
 
-ReactDOM.render(<App/>, document.getElementById("root"));
+ReactDOM.render(
+ <Router>
+     <React.StrictMode>
+        <App/>
+     </React.StrictMode>
+ </Router>,
+document.getElementById("root"));
