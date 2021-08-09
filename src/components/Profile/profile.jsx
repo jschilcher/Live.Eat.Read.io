be615681d4d3e6974affc654eed9fefb7c90000d
Jenelle <jschilcher8@gmail.com>
2021-08-09 18:06:12 -0500

    Add profile to app

--- src/components/Profile/profile.jsx
+++ src/components/Profile/profile.jsx
@@ -25,27 +25,7 @@ function Profile() {
                   <h1 className="profile-user-name">{`${breakdown}`}</h1>
     
                   <button className="btn profile-edit-btn">Edit Profile</button>
-    
-                  <button
-                    className="btn profile-settings-btn"
-                    aria-label="profile settings"
-                  >
-                    <i className="fas fa-cog" aria-hidden="true"></i>
-                  </button>
-                </div>
-    
-                <div className="profile-stats">
-                  <ul>
-                    <li>
-                      <span className="profile-stat-count">164</span> posts
-                    </li>
-                    <li>
-                      <span className="profile-stat-count">188</span> followers
-                    </li>
-                    <li>
-                      <span className="profile-stat-count">206</span> following
-                    </li>
-                  </ul>
+
                 </div>
     
                 <div className="profile-bio">
--- src/components/app.jsx
+++ src/components/app.jsx
@@ -4,6 +4,7 @@ import "./app.css"
 import Title from "./Title/title"
 import Login from "./Login/login";
 import Register from "./Register/register";
+import Profile from "./Profile/profile";
 
 function App() {
     return (
@@ -12,6 +13,7 @@ function App() {
             <Switch>
                 <Route path="/" exact component={Login}/>
                 <Route path="/register" component={Register}/>
+                <Route path="/profile" component={Profile}/>
             </Switch>
         </div>
     )
