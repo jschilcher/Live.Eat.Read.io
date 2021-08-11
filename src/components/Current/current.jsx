6e81409c86e4f15f89deda8e07804ad38ce3edd9
Jenelle <jschilcher8@gmail.com>
2021-08-10 20:14:39 -0500

    Current component setup

---
+++ src/components/Current/current.jsx
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import axios from "axios";
+
+const Current = () => {
+
+}
+
+export default Current;
